3e4a644aefb454e23d0db42ca489a2277df17450
Larry Cessna <larry.cessna@outlook.com>
2017-07-01 15:43:08 -0700

    Added chart for # of clicks. Working on adding chart for # of displays.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 'use strict';
 
-function Image(name, path) {
+var context = document.getElementById('image-chart').getContext('2d');
+
+function Image(name, path, color) {
   this.name = name;
   this.path = path;
+  this.color = color;
   this.clicks = 0;
   this.displays = 0;
   allImages.push(this);
@@ -12,27 +15,28 @@ function Image(name, path) {
 var allImages = [];
 var displayedImages = [];
 var selectionsRemaining = 25;
+var chartColors = ['black', 'white', 'brown', 'green', 'blue', 'red', 'black', 'white', 'brown', 'green', 'blue', 'red', 'black', 'white', 'brown', 'green', 'blue', 'red', 'black'];
 
-var bag = new Image('bag', 'img/bag.jpg');
-var banana = new Image('banana', 'img/banana.jpg');
-var bathroom = new Image('bathroom', 'img/bathroom.jpg');
-var boots = new Image('boots', 'img/boots.jpg');
-var breakfast = new Image('breakfast', 'img/breakfast.jpg');
-var bubblegum = new Image('bubblegum', 'img/bubblegum.jpg');
-var chair = new Image('chair', 'img/chair.jpg');
-var cthulhu = new Image('cthulhu', 'img/cthulhu.jpg');
-var dogduck = new Image('dogduck', 'img/dog-duck.jpg');
-var dragon = new Image('dragon', 'img/dragon.jpg');
-var pen = new Image('pen', 'img/pen.jpg');
-var petsweep = new Image('petsweep', 'img/pet-sweep.jpg');
-var scissors = new Image('scissors', 'img/scissors.jpg');
-var shark = new Image('shark', 'img/shark.jpg');
-var sweep = new Image('sweep', 'img/sweep.png');
-var tauntaun = new Image('tauntaun', 'img/tauntaun.jpg');
-var unicorn = new Image('unicorn', 'img/unicorn.jpg');
-var usb = new Image('usb', 'img/usb.gif');
-var watercan = new Image('watercan', 'img/water-can.jpg');
-var wineglass = new Image('wineglass', 'img/wine-glass.jpg');
+var bag = new Image('bag', 'img/bag.jpg', 'red');
+var banana = new Image('banana', 'img/banana.jpg', 'yellow');
+var bathroom = new Image('bathroom', 'img/bathroom.jpg', 'brown');
+var boots = new Image('boots', 'img/boots.jpg', 'orange');
+var breakfast = new Image('breakfast', 'img/breakfast.jpg', 'blue');
+var bubblegum = new Image('bubblegum', 'img/bubblegum.jpg', 'purple');
+var chair = new Image('chair', 'img/chair.jpg', 'light-blue');
+var cthulhu = new Image('cthulhu', 'img/cthulhu.jpg', 'green');
+var dogduck = new Image('dogduck', 'img/dog-duck.jpg', 'grey');
+var dragon = new Image('dragon', 'img/dragon.jpg', 'gold');
+var pen = new Image('pen', 'img/pen.jpg', 'red');
+var petsweep = new Image('petsweep', 'img/pet-sweep.jpg', 'yellow');
+var scissors = new Image('scissors', 'img/scissors.jpg', 'brown');
+var shark = new Image('shark', 'img/shark.jpg', 'orange');
+var sweep = new Image('sweep', 'img/sweep.png', 'blue');
+var tauntaun = new Image('tauntaun', 'img/tauntaun.jpg', 'purple');
+var unicorn = new Image('unicorn', 'img/unicorn.jpg', 'light-blue');
+var usb = new Image('usb', 'img/usb.gif', 'green');
+var watercan = new Image('watercan', 'img/water-can.jpg', 'grey');
+var wineglass = new Image('wineglass', 'img/wine-glass.jpg', 'gold');
 
 function showImages() {
   shuffle();
@@ -108,6 +112,46 @@ function finalResults() {
   allImages.sort(function (a, b) {
         return b.clicks - a.clicks;
       });
+  drawChart();
 }
 
-console.log(allImages);
+var imageLabels = [];
+var imageClicks = [];
+var imageDisplays = [];
+
+function drawChart(){
+  for (var i = 0; i < allImages.length; i++) {
+    imageLabels.push(allImages[i].name);
+  }
+
+  for (var i = 0; i < allImages.length; i++) {
+    imageClicks.push(allImages[i].clicks);
+  }
+
+  for (var i = 0; i < allImages.length; i++) {
+    imageDisplays.push(allImages[i].displays);
+
+  }
+
+  var imageChart = new Chart(context, {
+    type: 'bar',
+    data: {
+      labels: imageLabels,
+      datasets: [{
+        label: '# of Clicks',
+        data: imageClicks,
+        backgroundColor: chartColors,
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          }
+        }]
+      }
+    }
+  });
+
+}
